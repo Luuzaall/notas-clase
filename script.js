320aee0a484149b7444c602d6517db144e7bd4eb
Luuzaall <luuzaall@gmail.com>
2022-04-22 10:59:32 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,41 @@
+console.log('hello!');
+
+function saludar() {
+  var nombre = promp('Ingrese su nobre');
+  console.log(nombre);
+}
+/*
+Para definir una variable a nivel de àmbito donde estoy ubicado. Es decir, dentro de una función nomás.
+var variable
+
+
+variable = Number(variable) transformarlo en integer.
+
+resultado = numero >= 10 ? 'menor que 10' : 'mayor que 10'
+Para hacer un if en la misma línea. Es con ? y el :
+
+
+El == valua el contenido, no el tipo de dato. Si quiero además evaluar el tipo, poner ===
+
+con el pop hacemos como una burbuja oosea vemos el elemento que eliminamos ahr que nada que ver jiji jaja 
+Su out es el elemento eliminado
+
+array.sclice(2,1) Elimina el elemento del index 2 al index 1.
+
+El sumar(elem1, elem2) no sólo suma nros, si no strings tamb.
+var c = sumar;
+d = c(5,6)
+
+INTEROPERABILIDAD ENTRE APLICACIONES
+Son objetos con un formato de javascript. JSON reemplaza "xml".
+
+Los primeros corchetes indica que es un objeto.
+
+JQuery es un framework que fue muy fuerte pero ahora no se está utilizando más, pero la mayoría de las páginas lo tiene.
+Se dejó de usar al aplicar Angular, que las últimas versiones no lo están utilizando a JQuery
+onmouseover cuando el mouse entra al div. onmouseout cuando sale! El this hace referencia al dic.
+
+<button onclick="saludar()"></button> Paréntesis importante.
+`` es para un string multilínea 
+
+*/
